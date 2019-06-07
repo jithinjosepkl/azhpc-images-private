321b20d4d9aec0301a5e06d5c04a65cae5b594bf
Jithin Jose <jijos@microsoft.com>
2019-06-07 14:04:39 -0700

    Update install_ofed.sh

--- centos-7.6-hpc/install_ofed.sh
+++ centos-7.6-hpc/install_ofed.sh
@@ -5,24 +5,17 @@ set -x
 # Install Mellanox OFED
 mkdir -p /tmp/mlnxofed
 cd /tmp/mlnxofed
-wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.5-1.0.1.0/MLNX_OFED_LINUX-4.5-1.0.1.0-rhel7.6-x86_64.tgz
-tar zxvf MLNX_OFED_LINUX-4.5-1.0.1.0-rhel7.6-x86_64.tgz
+wget http://www.mellanox.com/downloads/ofed/MLNX_OFED-4.6-1.0.1.1/MLNX_OFED_LINUX-4.6-1.0.1.1-rhel7.6-x86_64.tgz
+tar zxvf MLNX_OFED_LINUX-4.6-1.0.1.1-rhel7.6-x86_64.tgz
 
 KERNEL=$(uname -r)
-./MLNX_OFED_LINUX-4.5-1.0.1.0-rhel7.6-x86_64/mlnxofedinstall --kernel-sources /usr/src/kernels/$KERNEL --add-kernel-support --skip-repo
+./MLNX_OFED_LINUX-4.6-1.0.1.1-rhel7.6-x86_64/mlnxofedinstall --kernel-sources /usr/src/kernels/$KERNEL --add-kernel-support --skip-repo
+cd && rm -rf /tmp/mlnxofed
 
 /etc/init.d/openibd restart
 cd && rm -rf /tmp/mlnxofed
 
-# Install WALinuxAgent
-mkdir -p /tmp/wala
-cd /tmp/wala
-wget https://github.com/Azure/WALinuxAgent/archive/v2.2.38.tar.gz
-tar -xvf v2.2.38.tar.gz
-cd WALinuxAgent-2.2.38
-python setup.py install --register-service --force
+# Configure WALinuxAgent
 sed -i -e 's/# OS.EnableRDMA=y/OS.EnableRDMA=y/g' /etc/waagent.conf
 sed -i -e 's/CGroups.EnforceLimits=n/CGroups.EnforceLimits=y/g' /etc/waagent.conf
 systemctl restart waagent
-cd && rm -rf /tmp/wala
-
